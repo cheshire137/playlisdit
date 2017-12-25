8be8169382ea7f8f15d2e9c246f648de646ade8a
Sarah Vessels <cheshire137@gmail.com>
2017-12-25 13:12:48 -0600

    Start on home view with router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PlaylistView from './components/PlaylistView'
 import 'bulma/css/bulma.css'
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Route exact path="/" component={PlaylistView} />
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/PlaylistView.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import UserForm from './UserForm'
+
+class PlaylistView extends Component {
+  render() {
+    return (
+      <div>
+        <section className="hero is-link">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Playlisdit</h1>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default PlaylistView
