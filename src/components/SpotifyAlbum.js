39580ea1843c1823a13cc0a8dcedd45f5f0b5ae9
Sarah Vessels <cheshire137@gmail.com>
2017-12-26 16:27:17 -0600

    Link to album artists

--- src/components/SpotifyAlbum.js
+++ src/components/SpotifyAlbum.js
@@ -3,10 +3,11 @@ import ExternalLink from './ExternalLink'
 import SpotifyLogo from './SpotifyLogo'
 import TracksList from './TracksList'
 import TrackCount from './TrackCount'
+import SpotifyArtist from './SpotifyArtist'
 
 class SpotifyAlbum extends Component {
   render() {
-    const { name } = this.props
+    const { name, artists } = this.props
     const tracks = this.props.tracks.items
     const url = this.props.external_urls.spotify
     const trackCount = this.props.tracks.total
@@ -20,8 +21,12 @@ class SpotifyAlbum extends Component {
           >
             <SpotifyLogo className="mr-1" />
             {name}
-            <TrackCount count={trackCount} />
           </ExternalLink>
+          <span> by </span>
+          {artists.map(artist => (
+            <SpotifyArtist {...artist} key={artist.id} />
+          ))}
+          <TrackCount count={trackCount} />
         </p>
         <TracksList
           tracks={tracks}
