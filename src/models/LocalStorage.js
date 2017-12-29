78733b12e8fd7ccc9c84c055ab33afdad9b63102
Sarah Vessels <cheshire137@gmail.com>
2017-12-29 17:09:29 -0600

    Check presence of window.localStorage on set, delete

--- src/models/LocalStorage.js
+++ src/models/LocalStorage.js
@@ -24,12 +24,16 @@ export default class LocalStorage {
   static set(key, value) {
     const appData = this.getJSON()
     appData[key] = value
-    window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+    if (window.localStorage) {
+      window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+    }
   }
 
   static delete(key) {
     const appData = this.getJSON()
     delete appData[key]
-    window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+    if (window.localStorage) {
+      window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+    }
   }
 }
